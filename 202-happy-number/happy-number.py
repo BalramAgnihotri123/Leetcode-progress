61d234a5f59ad5d3625bae4c199443b393ae45a1
Balram Agnihotri <balramagni19@gmail.com>
2025-07-20 17:43:22 +0530

    Time: 3 ms (60.12%) | Memory: 12.3 MB (98.75%) - LeetSync

--- 202-happy-number/happy-number.py
+++ 202-happy-number/happy-number.py
@@ -1,18 +1,18 @@
 class Solution(object):
     def isHappy(self, n):
-        seen = set()
+        seen = {}
         n = str(n)
 
         while True:
             isHappy = 0
             for s in n:
-                isHappy += int(s) * int(s)
-            
+                isHappy += int(s) ** 2
+
             if isHappy == 1:
                 return True
             elif isHappy in seen:
-                return False
-            else:
-                seen.add(isHappy)
-                n = str(isHappy)
+                return False                
+
+            seen[isHappy] = n
+            n = str(isHappy)
         
